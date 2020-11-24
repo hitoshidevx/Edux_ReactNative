fe58a0edc1f63dcba86cf8ea6e2edfc9eb456c56
hitoshi <g.hitoshi.yokogawa@gmail.com>
2020-11-24 19:37:11 -0300

    Login começado.
    Falta testar.

--- edux/App.js
+++ edux/App.js
@@ -2,12 +2,34 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+//Navigations
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+
+const Drawer = createDrawerNavigator();
+
+//Paginas
+import login from './pages/login';
+import Login from '../pages/login';
+
+const Autenticado = () =>{
+
+  return(
+    <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+    </Drawer.Navigator>
+  )
+
+}
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown : false }}>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Autenticado" component={Autenticado} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ pages/login/index.js
@@ -0,0 +1,129 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, KeyboardAvoidingView, Platform, View, TextInput, TouchableOpacity, Image, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { url } from '../../utils/constants'
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const Login = () =>{
+
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
+
+    const salvar = async (value) => {
+        try {
+            await AsyncStorage.setItem('@jwt', value)
+        } catch (e) {
+            // saving error
+        }
+    }
+
+    const Logar = () => {
+
+        const corpo = {
+            email: email,
+            senha: senha
+        }
+
+        fetch(`${url}login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(corpo)
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                if (data.status != 404) {
+                    alert('Bem vindo!!');
+
+                    salvar(data.token);
+                    navigation.push('Autenticado');
+                } else {
+                    alert('Email ou senha inválidos... Tente novamente');
+                }
+            })
+
+    }
+    
+    return (
+        <KeyboardAvoidingView
+            behavior={Platform.OS == "ios" ? "padding" : "height"}
+            style={styles.container}
+        >
+            
+                <View style={styles.container}>
+                    <Text style={{fontWeight: 1000, fontSize: 80, color: 'white'}}>Edux</Text>
+                    <Text style={styles.textoHeader}>LOGIN</Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Email"
+                        onChangeText={text => setEmail(text)}
+                        value={email}
+                       
+                       
+                    />
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Senha"
+                        onChangeText={text => setSenha(text)}
+                        value={senha}
+                        secureTextEntry={true}
+                    />
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={Logar}
+                    >
+                        <Text style={styles.textButton}>Entrar</Text>
+                    </TouchableOpacity>
+                </View>
+           
+        </KeyboardAvoidingView>
+    )
+
+}
+
+export default Login;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#000',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    textoHeader: {
+        color: 'white',
+        margin: 10,
+        fontWeight: 700,
+        marginBottom: 15,
+        fontSize: 24
+    },
+    input: {
+        width: 250,
+        height: 40,
+        color: '#000',
+        backgroundColor: 'white',
+        borderColor: 'white',
+        borderWidth: 1,
+        marginTop: 20,
+        padding: 5,
+        paddingLeft: 10,
+        borderRadius: 10,
+        fontWeight: 1000
+    },
+    button: {
+        backgroundColor: 'white',
+        width: 250,
+        padding: 10,
+        borderRadius: 10,
+        marginTop: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    textButton: {
+        fontWeight: 1000,
+        color: 'black'
+    }
+
+});
